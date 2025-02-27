5b33228f8f6b05e49b4a13887781af3d74fe44da
LucasJBultman <142909649+LucasJBultman@users.noreply.github.com>
2025-02-27 11:07:38 -0600

    Update Game.java
    big

--- Game.java
+++ Game.java
@@ -3,13 +3,14 @@ public class Game {
     protected int number; 
     protected int startingNum;
     protected int lastMove;
-    private Scanner input = new Scanner();
+    private Scanner bigBrother = new Scanner(); // He is always listening
     public void gameLoop(){
+        
         while(true){
             if(number <= 0){
                 break;
             }
-            String nextMove = myObj.nextLine();
+            String nextMove = bigBrother.nextLine();
             System.out.print("The current Nim is "+ number + "What do you want to subtract by?");
 
         }
