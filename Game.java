028981122ff55831436183b72c8088508f0601b6
Uneevee <102497235+Uneevee@users.noreply.github.com>
2025-02-27 10:58:34 -0600

    Update Game.java

--- Game.java
+++ Game.java
@@ -1,9 +1,11 @@
 public class Game {
     public int number; 
+    public int startingNum;
+    public int lastMove;
     public void gameLoop(){
         while(true){
             if(number <= 0){
-                break
+                break;
             }
         }
     }
