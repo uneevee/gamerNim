8369f4a0c8d00722d21b001e44d20fe1b6d9db15
Uneevee <102497235+Uneevee@users.noreply.github.com>
2025-02-28 15:41:01 -0600

    Text.java now has stuff in it

--- Text.java
+++ Text.java
@@ -1,5 +1,14 @@
 public class Text extends Game{
 	//Mace's
 	private Scanner bigBrother = new Scanner(System.in); // He is always listening
-	public
+	public boolean startGame(){
+		System.out.println()
+		String input = bigBrother.nextLine();
+		gameLoop(input.toInt());
+		return endGame(); //if true, play again.
+	}
+	//Suda
+	private boolean endGame(){
+		//TODO: End the game, change winLoss for each player, ask to play again
+	}
 }
